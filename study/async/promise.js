f275b4d92c6e377cc519d4797e5e3a70920ca10f
pointehd <pointehd@naver.com>
2022-05-11 22:43:26 +0900

    Promise 공부
    Promise 공부
    기존 promise 파일의 이름이 적절핳지않아callback.js 로 변경

--- study/async/promise.js
+++ study/async/promise.js
@@ -1,36 +1,42 @@
+const prom = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+    // reject(new Error("error"));
+  }, 1000);
+});
+
+prom.then((count) => console.log(count)).catch(console.error); // equals (error) => console.error(error);
+
+// callback 지옥 코드 리펙토링
 class CallUser {
-  getUser(id, pwd, resolve, reject) {
-    setTimeout(() => {
-      if (id === "dong" && pwd === "1234") {
-        resolve(1);
-      } else {
-        reject(new Error("not foundUser"));
-      }
-    }, 1000);
+  getUser(id, pwd) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (id === "dong" && pwd === "1234") {
+          resolve(1);
+        } else {
+          reject(new Error("not foundUser"));
+        }
+      }, 1000);
+    });
   }
-  getList(seq, resolve, reject) {
-    console.log(seq);
-    if (seq === 1) {
-      setTimeout(() => resolve(["ok", "no"]), 2000);
-    } else {
-      reject(new Error("not found list"));
-    }
+  getList(seq) {
+    return new Promise((resolve, reject) => {
+      if (seq === 1) {
+        setTimeout(() => resolve(["ok", "no"]), 2000);
+      } else {
+        reject(new Error("not found list"));
+      }
+    });
   }
 }
 
-// callback 지옥 체험하기
-const callUser = new CallUser();
-const id = prompt("what is your id?");
-const pwd = prompt("what is your pwd?");
-callUser.getUser(
-  id,
-  pwd,
-  (user) => {
-    callUser.getList(
-      user,
-      (list) => alert(list),
-      (error) => console.error(error)
-    );
-  },
-  (error) => console.error(error)
-);
+const promiseUser = new CallUser();
+const id = prompt("id 입력");
+const pwd = prompt("pwd 입력");
+promiseUser
+  .getUser(id, pwd)
+  //   .then((seq) => seq * 2) // 메서드 체인처럼 사용가능
+  .then((seq) => promiseUser.getList(seq))
+  .then((list) => alert(list))
+  .catch((error) => console.error(error));
