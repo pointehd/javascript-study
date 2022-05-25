f03319ba64944c295e37346bab579592ae56809c
pointehd <pointehd@naver.com>
2022-05-25 20:33:27 +0900

    javascript primitive, object type
    javascript primitive, object type 공부
    엘리님 강의 보고 따라하기

---
+++ study/primitive-object.js
@@ -0,0 +1,22 @@
+// pri number, string, boolean, null, undefined, symbol(잘사용하지않음)
+let number = 2;
+let number2 = number;
+
+console.log(number);
+console.log(number2);
+
+number2 = 3;
+
+console.log(number);
+console.log(number2);
+
+console.clear();
+
+// object
+
+let obj = {
+  name: "ellie",
+  age: 5,
+};
+
+//기초 지식...
